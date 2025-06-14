bb9c720fbf8d06420939125badb4f5770edac7ec
Dennis <itgenius1004@gmail.com>
2025-06-14 08:14:37 +0000

    ok

--- my-app/src/main/java/com/test/my_app/MyAppApplication.java
+++ my-app/src/main/java/com/test/my_app/MyAppApplication.java
@@ -1,7 +1,10 @@
 package com.test.my_app;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyAppApplication {
@@ -9,4 +12,12 @@ public class MyAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
 	}
+
+    @Bean
+    public Function<String, String> uppercase() {
+        return message -> {
+            System.out.println("Received: " + message);
+            return message.toUpperCase();
+        };
+    }
 }
