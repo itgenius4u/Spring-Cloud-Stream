726591ac333b7cb33f6afb79d7c454fa1fd576c4
Dennis <itgenius1004@gmail.com>
2025-06-15 02:30:58 +0000

    ok

--- producer-app2/src/main/java/com/test/producer_app2/ProducerApp2Application.java
+++ producer-app2/src/main/java/com/test/producer_app2/ProducerApp2Application.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProducerApp2Application {
