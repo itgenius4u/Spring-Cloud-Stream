c1c582a2113a1de3dde49a2bdcb730b590f08d2c
Dennis <itgenius1004@gmail.com>
2025-06-15 04:22:30 +0000

    ok

---
+++ consumer-app2/src/main/java/com/test/consumer_app2/ConsumerApp2Application.java
@@ -0,0 +1,13 @@
+package com.test.consumer_app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApp2Application.class, args);
+	}
+
+}
---
+++ consumer-app2/src/test/java/com/test/consumer_app2/ConsumerApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.consumer_app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
