27cbdd0ca9750ddbdb12db4a1d058b43bb2b9e2e
Dennis <itgenius1004@gmail.com>
2025-06-15 01:50:31 +0000

    ok

--- consumer-app/src/main/java/com/test/consumer_app/ConsumerAppApplication.java
+++ consumer-app/src/main/java/com/test/consumer_app/ConsumerAppApplication.java
@@ -1,7 +1,10 @@
 package com.test.consumer_app;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConsumerAppApplication {
@@ -9,5 +12,8 @@ public class ConsumerAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerAppApplication.class, args);
 	}
-
+   	@Bean
+    public Consumer<String> receive() {
+        return message -> System.out.println(">>> Received: " + message);
+    }
 }
