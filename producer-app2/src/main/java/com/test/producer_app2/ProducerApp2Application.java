fb9451bcbe25a2b06ebc5f219c496a3d61e1f644
Dennis <itgenius1004@gmail.com>
2025-06-15 02:22:38 +0000

    ok

---
+++ producer-app2/src/main/java/com/test/producer_app2/ProducerApp2Application.java
@@ -0,0 +1,13 @@
+package com.test.producer_app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApp2Application.class, args);
+	}
+
+}
---
+++ producer-app2/src/test/java/com/test/producer_app2/ProducerApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.producer_app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
