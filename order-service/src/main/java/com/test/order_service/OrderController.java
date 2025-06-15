30b4e4d8f95e10cd874e4648a0813deede2fcf78
Dennis <itgenius1004@gmail.com>
2025-06-15 06:24:10 +0000

    ok

--- order-service/src/main/java/com/test/order_service/OrderController.java
+++ order-service/src/main/java/com/test/order_service/OrderController.java
@@ -3,6 +3,7 @@ package com.test.order_service;
 import java.util.UUID;
 
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
 
     private final StreamBridge streamBridge;
+
+    public interface OrderRepository extends JpaRepository<OrderCreated, String> {}
 
     @PostMapping("/orders")
     public ResponseEntity<String> createOrder(@RequestParam String productName) {
