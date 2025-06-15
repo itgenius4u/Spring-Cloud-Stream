4fada3a0dae927af7e4c0b7b031934446b521374
Dennis <itgenius1004@gmail.com>
2025-06-15 06:02:48 +0000

    ok

--- order-service/src/main/java/com/test/order_service/OrderCreated.java
+++ order-service/src/main/java/com/test/order_service/OrderCreated.java
@@ -1,6 +1,8 @@
 package com.test.order_service;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +16,9 @@ import lombok.Setter;
 @Entity
 @Table(name = "orders")
 public class OrderCreated {
+    @Id
+    @Column(name = "order_id")
     private String orderId;
+    @Column(name = "product_name")
     private String productName;
 }
