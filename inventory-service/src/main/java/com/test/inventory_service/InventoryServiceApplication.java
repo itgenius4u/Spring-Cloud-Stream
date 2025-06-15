b5647bb96ef73ad2770e3e4e40b8528437022602
Dennis <itgenius1004@gmail.com>
2025-06-15 06:44:07 +0000

    ok

--- inventory-service/src/main/java/com/test/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/test/inventory_service/InventoryServiceApplication.java
@@ -1,13 +1,20 @@
 package com.test.inventory_service;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+	@Bean
+	public Consumer<OrderCreated> inventoryConsumer() {
+		return order -> System.out.println("재고 차감: " + order.getOrderId());
 	}
 
 }
---
+++ inventory-service/src/main/java/com/test/inventory_service/OrderCreated.java
@@ -0,0 +1,16 @@
+package com.test.inventory_service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreated {
+  private String orderId;
+  private String productName;
+  // getters, setters, constructor
+}
