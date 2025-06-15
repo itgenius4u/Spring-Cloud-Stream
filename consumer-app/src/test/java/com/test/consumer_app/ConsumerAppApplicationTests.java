09f89657361dbe723ea6bf4f56bf826b06f6873a
Dennis <itgenius1004@gmail.com>
2025-06-15 01:45:58 +0000

    ok

---
+++ consumer-app/src/test/java/com/test/consumer_app/ConsumerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.consumer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
